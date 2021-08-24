599f3e3ab78557e6e482fdff076c3de8d7a0fe12
kelsimhoyle <kelsimhoyle@gmail.com>
2021-08-24 11:05:30 -0600

    header img

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react'
 import { Link } from 'gatsby';
-import { StaticImage } from "gatsby-plugin-image"
+import { getImage, GatsbyImage } from "gatsby-plugin-image"
 import { MdMenu } from "@react-icons/all-files/md/MdMenu";
 import { MdClose } from "@react-icons/all-files/md/MdClose";
 import { Popover, Transition } from '@headlessui/react'
@@ -11,7 +11,7 @@ import { navigate } from '@reach/router'
 import { logout, isLoggedIn } from "../utils/auth"
 import { Auth } from 'aws-amplify'
 
-const Header = ({ siteTitle, pages }) => (
+const Header = ({ siteTitle, headerImg, mobileImg, pages }) => (
   <Popover>
     {({ open }) => (
       <>
@@ -24,7 +24,12 @@ const Header = ({ siteTitle, pages }) => (
               <div className="flex items-center justify-between w-full md:w-auto">
                 <a href="/">
                   <span className="sr-only">James the Dog Walker</span>
-                  <StaticImage src="../images/James.png" height="100" />
+                  <GatsbyImage
+                    image={headerImg}
+                    objectFit="contain"
+                    alt="James the Dog Walker"
+                    className="max-h-24 md:max-h-80"
+                  />
 
                 </a>
                 <div className="-mr-2 flex items-center md:hidden">
@@ -37,10 +42,10 @@ const Header = ({ siteTitle, pages }) => (
             </div>
             <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
               {pages.map((item) => (
-              <Link key={item.name} to={item.href} className="font-medium text-gray-500 hover:text-gray-900">
-                {item.name}
-              </Link>
-            ))}
+                <Link key={item.name} to={item.href} className="font-medium text-gray-500 hover:text-gray-900">
+                  {item.name}
+                </Link>
+              ))}
               {
                 isLoggedIn() && (
                   <p
@@ -73,8 +78,12 @@ const Header = ({ siteTitle, pages }) => (
             <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
               <div className="px-5 pt-4 flex items-center justify-between">
                 <div>
-                <FaPaw className="text-4xl text-green-400" />
+                  <GatsbyImage
+                    image={mobileImg}
+                    alt="James the Dog Walker"
+                    imgClassName="h-1.5	"
 
+                  />
                 </div>
                 <div className="-mr-2">
                   <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -84,17 +93,17 @@ const Header = ({ siteTitle, pages }) => (
                 </div>
               </div>
               <div className="px-2 pt-2 pb-3 space-y-1">
-                 {pages.map((item) => (
-                <Link
-                  key={item.name}
-                  to={item.href}
-                  className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                >
-                  {item.name}
-                </Link>
-              ))} 
+                {pages.map((item) => (
+                  <Link
+                    key={item.name}
+                    to={item.href}
+                    className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                  >
+                    {item.name}
+                  </Link>
+                ))}
               </div>
-             
+
 
             </div>
           </Popover.Panel>
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-
+import { getImage } from 'gatsby-plugin-image';
 import Header from "../components/header"
 import Footer from "../components/Footer";
 // import './layout.css'
@@ -10,17 +10,39 @@ import Footer from "../components/Footer";
 const Layout = ({ children, data }) => (
   <StaticQuery
     query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-            pages {
-              name
-              href
+    query SiteTitleQuery {
+      site: site {
+        siteMetadata {
+          title
+          pages {
+            name
+            href
+          }
+        }
+      }
+      header: airtable(table: {eq: "Website Data"}, data: {Name: {eq: "Header Logo"}}) {
+        data {
+          Image {
+            localFiles {
+              childImageSharp {
+                gatsbyImageData(height: 125, width: 300)
+              }
             }
           }
         }
       }
+      mobileHeader: airtable(table: {eq: "Website Data"}, data: {Name: {eq: "Logo"}}) {
+        data {
+          Image {
+            localFiles {
+              childImageSharp {
+                gatsbyImageData(width: 100)
+              }
+            }
+          }
+        }
+      }
+    }
     `}
     render={data => (
       <>
@@ -33,7 +55,12 @@ const Layout = ({ children, data }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} pages={data.site.siteMetadata.pages} />
+        <Header
+          siteTitle={data.site.siteMetadata.title}
+          pages={data.site.siteMetadata.pages}
+          headerImg={getImage(data.header.data.Image.localFiles[0].childImageSharp)}
+          mobileImg={getImage(data.mobileHeader.data.Image.localFiles[0].childImageSharp)}
+        />
         {children}
         <Footer pages={data.site.siteMetadata.pages} />
       </>
