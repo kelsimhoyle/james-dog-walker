1ca57a732e028bdcb71368ded8834d3b7a269ce5
kelsimhoyle <kelsimhoyle@gmail.com>
2021-08-26 09:36:36 -0600

    fixed contact form errors

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -6,12 +6,15 @@ import * as Yup from 'yup';
 import addContact from "../functions/addContact";
 import ConfirmModal from "./ConfirmModal";
 
+
+const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
 const ContactSchema = Yup.object().shape({
     Name: Yup.string()
         .min(2, 'Too Short!')
         .required('Required'),
-    Email: Yup.string().email('Invalid email').required('Required'),
-    Tel: Yup.string().matches(new RegExp('[0-9]{7}')),
+    Email: Yup.string().email("Please enter a valid email address.").required('Please enter an email address.'),
+    Tel: Yup.string().matches(phoneRegExp, "Please enter a valid phone number."),
     Message: Yup.string()
 });
 
@@ -34,22 +37,22 @@ const ContactForm = () => {
             <Formik
                 initialValues={{ Name: "", Email: "", Message: "", Tel: "" }}
                 validationSchema={ContactSchema}
-
                 onSubmit={(values) => {
                     handleSubmit(values)
                 }}
             >
-                {({ isSubmitting, errors }) => (
+                {({ isSubmitting, errors, touched }) => (
                     <Form
                         className="p-6 flex flex-col justify-center bg-white rounded-b-lg md:rounded-r-lg md:rounded-b-none">
                         <div className="flex flex-col">
                             <Field
-                                type="name"
+                                type="text"
                                 name="Name"
                                 id="Name"
                                 placeholder="Name"
+                                className={`${errors.Name && touched.Name ? "border-red-500" : "border-gray-400"} w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border  dark:border-gray-700 text-gray-800 font-semibold focus:border-green-500 focus:outline-none`} />
+                            <ErrorMessage name="Name" component="div" className="text-red-500 font-medium ml-1 mt-1" />
 
-                                className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-green-500 focus:outline-none" />
                         </div>
 
                         <div className="flex flex-col mt-2">
@@ -59,9 +62,8 @@ const ContactForm = () => {
                                 id="Email"
                                 placeholder="Email"
 
-                                className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-green-500 focus:outline-none" />
-                            <ErrorMessage name="Email" component="div" />
-
+                                className={`${errors.Email && touched.Email ? "border-red-500" : "border-gray-400"} w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-green-500 focus:outline-none`} />
+                            <ErrorMessage name="Email" component="div" className="text-red-500 font-medium ml-1 mt-1" />
                         </div>
 
                         <div className="flex flex-col mt-2">
@@ -70,20 +72,20 @@ const ContactForm = () => {
                                 name="Tel"
                                 id="Tel"
                                 placeholder="Telephone Number"
-
-                                className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-green-500 focus:outline-none" />
-                            <ErrorMessage name="Tel" component="div" />
+                                className={`${errors.Tel && touched.Tel ? "border-red-500" : "border-gray-400"} w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-green-500 focus:outline-none`} />
+                            <ErrorMessage name="Tel" component="div" className="text-red-500" />
 
                         </div>
 
                         <div className="flex flex-col mt-2">
                             <Field
-                                type="text"
+
+                                as="textarea"
+                                rows="3"
                                 name="Message"
                                 id="Message"
                                 placeholder="Message"
-
-                                className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-green-500 focus:outline-none" />
+                                className="w-100 mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-800 font-semibold focus:border-green-500 focus:outline-none resize-none" />
                         </div>
 
                         <button type="submit" className="md:w-32 bg-green-600 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-green-500 transition ease-in-out duration-300 disabled:opacity-50"
--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -25,7 +25,7 @@ const ContactInfo = ({phone, email, location, instagram}) => {
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
                 </svg>
                 <div className="ml-4 text-md tracking-wide font-semibold w-60 hover:text-green-500">
-                    <a href={`sms://+${phone}`}>Text Message</a>
+                    <a href={`sms://+1${phone}`}>{phone.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3")}</a>
                 </div>
             </div>
 
