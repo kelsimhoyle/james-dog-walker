2b11f238bd2d76d495ef45a2c6a828453ad99345
kelsimhoyle <kelsimhoyle@gmail.com>
2021-09-08 10:04:29 -0600

    adding SEO to blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,37 +1,42 @@
 import React from "react";
 import { graphql } from "gatsby";
 import BlogPost from "../components/BlogPost";
+import Seo from "../components/Seo";
 
 const Blog = ({ data }) => {
   const posts = data.posts.edges;
-  console.log(posts)
-
 
   return (
-    <div className="container  w-full text-xl md:text-2xl text-gray-800 leading-normal rounded-t px-4 md:px-0 max-w-6xl mx-auto mt-4">
-      <h1 className="text-2xl text-center tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl pt-10">
-        Blog
-      </h1>
-      {posts.length > 0 ?
-        (posts.map(post => (
-          <BlogPost
-            name={post.node.data.Name}
-            image={post.node.data.Images.localFiles[0]}
-            excerpt={post.node.data.Post.childMarkdownRemark.excerpt}
-            href={`/blog/${post.node.data.Name.trim().toLowerCase().split(" ").join("-")}`}
-          />
-        ))
-        ) :
-        (
-          <BlogPost 
-            name="Blog Coming Soon"
-            image={data.image.data.Image.localFiles[0]}
-            excerpt="I am working on blog posts of my adventures with my doggos!"
-          />
-        )
-      }
+    <>
+      <Seo
+        title="James the Dog Walker - Blog"
+      />
+      <div className="container  w-full text-xl md:text-2xl text-gray-800 leading-normal rounded-t px-4 md:px-0 max-w-6xl mx-auto mt-4">
+        <h1 className="text-2xl text-center tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl pt-10">
+          Blog
+        </h1>
+        {posts.length > 0 ?
+          (posts.map(post => (
+            <BlogPost
+              key={post.node.data.Name}
+              name={post.node.data.Name}
+              image={post.node.data.Images.localFiles[0]}
+              excerpt={post.node.data.Post.childMarkdownRemark.excerpt}
+              href={`/blog/${post.node.data.Name.trim().toLowerCase().split(" ").join("-")}`}
+            />
+          ))
+          ) :
+          (
+            <BlogPost
+              name="Blog Coming Soon"
+              image={data.image.data.Image.localFiles[0]}
+              excerpt="I am working on blog posts of my adventures with my doggos!"
+            />
+          )
+        }
 
-    </div>
+      </div>
+    </>
   )
 }
 
--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -1,27 +1,30 @@
 import React from "react";
 import { Link } from "gatsby";
-import  { FaArrowLeft } from "@react-icons/all-files/fa/FaArrowLeft"
+import { FaArrowLeft } from "@react-icons/all-files/fa/FaArrowLeft"
+import Seo from "../components/Seo";
 
-
-const BlogPost = ({pageContext: { title, post, keywords}}) => {
+const BlogPost = ({ pageContext: { title, post, keywords } }) => {
     return (
-        <div className="container max-w-5xl mx-auto">
+        <>
+            <Seo
+                title={title}
+                keywords={keywords}
+            />
+            <div className="container max-w-5xl mx-auto">
+                <div className="bg-white w-full p-8 md:p-24 text-xl md:text-2xl text-gray-800 leading-normal mt-4" >
+                    <h3 className="uppercase text-2xl md:text-3xl mb-5 text-green-500 font-bold">{title}</h3>
+                    <div
+                        className="mt-4 text-gray-600 space-y-6"
+                        dangerouslySetInnerHTML={{
+                            __html: post.childMarkdownRemark.html,
+                        }}
+                    />
 
-
-            <div className="bg-white w-full p-8 md:p-24 text-xl md:text-2xl text-gray-800 leading-normal mt-4" >
-                <h3 className="uppercase text-2xl md:text-3xl mb-5 text-green-500 font-bold">{title}</h3>
-                <div
-                    className="mt-4 text-gray-600 space-y-6"
-                    dangerouslySetInnerHTML={{
-                        __html: post.childMarkdownRemark.html,
-                    }}
-                />
-
+                </div>
+                <Link to="/blog" className="text-xl text-gray-500 hover:text-gray-900 flex items-center m-10"><FaArrowLeft /> View All Posts</Link>
             </div>
-            <Link to="/blog" className="text-xl text-gray-500 hover:text-gray-900 flex items-center m-10"><FaArrowLeft  /> View All Posts</Link>
-
-    </div>
-    )
-}
+        </>
+    );
+};
 
 export default BlogPost;
